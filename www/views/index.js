053f0e3e99080f1929a42313d5d2664268d72692
Daniel Du <duchangyu@gmail.com>
2015-03-02 20:16:33 +0800

    add subtool bar

--- www/views/index.js
+++ www/views/index.js
@@ -15,17 +15,20 @@
 // DOES NOT WARRANT THAT THE OPERATION OF THE PROGRAM WILL BE
 // UNINTERRUPTED OR ERROR FREE.
 /////////////////////////////////////////////////////////////////////////////////
-var urnprod = 'dXJuOmFkc2sub2JqZWN0czpvcy5vYmplY3Q6YWRuLXZpZXdlci1nYWxsZXJ5LzBjMGMtMThiZi02MjJmLWNhNWUtOWVmMi5kd2Y=';
+var urnprod = 'dXJuOmFkc2sub2JqZWN0czpvcy5vYmplY3Q6bW9kZWwtYXMtYS1zZXJ2aWNlMjAxNTAxMjYvU3VzcGVuc2lvbi5kd2Y=';
 var urnstg = 'dXJuOmFkc2sub2JqZWN0czpvcy5vYmplY3Q6YWRuLTEyLjAyLjIwMTUtMTMuNTUuMDIvQW5hbHl6ZS5kd2Y=';
+
+var _viewer = null;
 
 $(document).ready(function () {
 
     // indicates if using staging or production environment
     var staging = false;
 
-    var tokenurl = 'http://' + window.location.host +
-        '/node/basic/api/' + (staging ? 'tokenstg' : 'token');
+    // var tokenurl = 'http://' + window.location.host +
+    //     '/node/basic/api/' + (staging ? 'tokenstg' : 'token');
 
+    var tokenurl = 'http://still-spire-1606.herokuapp.com/api/rawtoken';
     var config = {
 
         environment : (staging ? 'AutodeskStaging' : 'AutodeskProduction')
@@ -48,6 +51,135 @@ $(document).ready(function () {
 function onViewerInitialized(viewer)
 {
     console.log('Viewer initialized');
+
+    _viewer = viewer;
+
+    // custom toobar config 
+    var toolbarConfig = {
+            //array of toolbars, this configration contains multi-toolbars
+          'toolbars': [ 
+            {
+                // id of toolbar
+              'id': 'toolbar_viewerSubToolbar_1',
+              // position of toolbar
+              // possible values 'viewer_subToolbar' | 'viewer_canvas' | 'custom_canvas'
+              'toolbar_type': 'viewer_subToolbar',
+              // css classname of toolbar position, required for | 'viewer_canvas' | 'custom_canvas'
+              'position_css_class': '',
+              // array of buttons
+              'buttons': [
+                {
+                  'id': 'button1',
+                  'buttonText': 'Rotation',
+                  'tooltip': 'Ratate the model at X direction',
+                  'visible': true,
+                  'backgroundImage': './res/icons/lmvdbg1.png',
+                  'onClick': function(e){
+                     alert('button1 is clicked.');
+
+                  }
+                },
+                {
+                  'id': 'button2',
+                  'buttonText': 'explode',
+                  'tooltip': 'Explode the model',
+                  //'visible': false, //default is true
+                  'backgroundImage': './res/icons/lmvdbg2.png',
+                  'onClick': function(e){
+                     alert('button2 is clicked.');
+
+                  }
+                }
+              ]
+            },
+            {
+                //Id of toolbar
+              'id': 'toolbar_viewerCanvas_1',
+              // position of toolbar
+              // possible values 'viewer_subToolbar' | 'viewer_canvas' | 'custom_canvas'
+              'toolbar_type': 'viewer_canvas',
+              // css classname of toolbar position, required for | 'viewer_canvas' | 'custom_canvas'
+              'position_css_class': '',
+              // array of buttons
+              'buttons': [
+                {
+                  'id': 'button3',
+                  'buttonText': 'button3',
+                  'tooltip': 'tooltip for button3',
+                  'visible': true,
+                  'backgroundImage': './res/icons/lmvdbgSolid.png',
+                  'onClick': function(e){
+                     alert('button3 is clicked.');
+
+                  }
+                },
+                {
+                  'id': 'button4',
+                  'buttonText': 'button4',
+                  'tooltip': 'tooltip for button4',
+                  'visible': true,
+                  'backgroundImage': './res/icons/lmvdbgGradient.png',
+                  'onClick': function(e){
+                     alert('button4 is clicked.');
+
+                  }
+                }
+              ]
+            },
+            {
+                //Id of toolbar
+              'id': 'toolbar_custom_cancas_1',
+              // position of toolbar
+              // possible values 'viewer_subToolbar' | 'viewer_canvas' | 'custom_canvas'
+              'toolbar_type': 'custom_canvas',
+              // css classname of toolbar position, required for | 'viewer_canvas' | 'custom_canvas'
+              'position_css_class': '',
+              // array of buttons
+              'buttons': [
+                {
+                  'id': 'button5',
+                  'buttonText': 'Rotation',
+                  'tooltip': 'tooltip of button5',
+                  'visible': true,
+                  'backgroundImage': 'Images/3d_rotation.png',
+                  'onClick': function(e){
+                     alert('button4 is clicked.');
+
+                  }
+                },
+                {
+                  'id': 'button6',
+                  'buttonText': 'Rotation',
+                  'tooltip': 'tooltip of button6',
+                  'visible': true,
+                  'backgroundImage': 'Images/3d_rotation.png',
+                  'onClick': function(e){
+                     alert('button4 is clicked.');
+
+                  }
+                }
+              ]
+            }
+          ]
+        };
+
+    var options = {
+        'toolbarConfig':toolbarConfig
+
+    };
+
+    //load the toolbar extension
+    viewer.loadExtension('Autodesk.ADN.Viewing.Extension.ToolbarExt', options);
+
+    //viewer.loadExtension('Autodesk.ADN.Viewing.Extension.Toolbar', options);
+
+
+
+
+
+
+
+    
 };
 
 function onError(error)
