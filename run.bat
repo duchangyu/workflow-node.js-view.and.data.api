1e30982a42a605a634e87afd7a159a42662efa38
Daniel Du <duchangyu@gmail.com>
2015-05-13 13:12:34 +0800

    remove local test run script

--- run.bat
+++
@@ -1,4 +0,0 @@
-set ConsumerKey=<replace_with_your_consummer_key> 
-set ConsumerSecret=<replace_with_your_secret_key> 
-set BaseUrl=https://developer.api.autodesk.com 
-node server.js
--- run.sh
+++
@@ -1,4 +0,0 @@
-ConsumerKey=<replace_with_your_consummer_key>  \
-ConsumerSecret=<replace_with_your_secret_key> \
-BaseUrl=https://developer.api.autodesk.com \
-node server.js
